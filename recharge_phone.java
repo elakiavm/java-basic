17fcbc82ffcb1a45030e14bcc546b8e8a2ea3982
elakia <vmelakia@gmail.com>
2022-04-28 12:59:31 +0530

    updated programs

--- recharge_phone.java
+++ recharge_phone.java
@@ -3,15 +3,20 @@ public class recharge_phone {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter the nos of day for the rechagre plan");
-        int nos_days=input.nextInt();
+        System.out.println("Enter the recharge plan");
+        int recharge_plan=input.nextInt();
 
-        if(nos_days==84){
+        if(recharge_plan==84){
             System.out.println("Enter the nos of call made");
             int nos_calls = input.nextInt();
 
             System.out.println("Enter the no of sms made ");
-            
+            int nos_sms = input.nextInt();
+
+            System.out.println("Enter the no of data used");
+            int data = input.nextInt();
+
+
 
         }
         else{
--- string_program.java
+++ string_program.java
@@ -18,8 +18,31 @@ string is immutable part
 import java.util.*;
 public class string_program {
     public static void main(String[] args){
+        
+        //stack
         char letter[] = {'a','b','c','d','e'};
         System.out.println(letter);
+
+        //non-pool
+        String mec = new String("Hello");
+        System.out.println(mec);
+
+        //pool
+        String name = "elakia";
+        System.out.println(name);
+
+        String pets = "Dog,Cat,Cow,Rat";
+        System.out.println(pets.toLowerCase());
+        System.out.println(pets);
+        System.out.println(pets.toUpperCase());
+        System.out.println(pets.concat(",Monkey"));
+        System.out.println(pets);
+        System.out.println(pets.indexOf('a'));
+        System.out.println(pets.charAt(5));
+        System.out.println(pets);
+
+
+
 
     }
 }
