e2508b5f86e4408c8cb3d9bd68d1089df8fdf714
elakia <vmelakia@gmail.com>
2022-12-13 10:03:04 +0530

    added adv face todays work

--- adv-face/assembleOrder.java
+++ adv-face/assembleOrder.java
@@ -21,36 +21,58 @@ public class assembleOrder {
             }
         }
         System.out.println("\n\n");
-        for(int j=0;j<pcount;j++){
-            System.out.println(b[j]);
-        }
+        // for(int j=0;j<pcount;j++){
+        //     System.out.println(b[j]);
+        // }
+        // System.out.println("\n\n");
+        // for(int j=0;j<ncount;j++){
+        //     System.out.println(a[j]);
+        // }
+        System.out.println(ncount);
         System.out.println("\n\n");
-        for(int j=0;j<ncount;j++){
-            System.out.println(a[j]);
-        }
-        boolean flag = false;
-        int sp=0,sn=0,i=0;
-        while(sp<=pcount && sn<=ncount)
-        {
-            if(flag==true){
-                arr[i]=a[i];
-                sn++;
-                i++;
-                flag=false;
-            }
-            else{
-                arr[i]=b[i];
-                sp++;
-                i++;
-                flag=true;
-            }
-        }
+        System.out.println(pcount);
         System.out.println("\n\n");
-        System.out.println(i);
-        System.out.println("\n\n");
-        for(int j=0;j<n;j++){
-            System.out.println(arr[j]);
-        }
+        // boolean flag = false;
+        // int sp=0,sn=0,i=0;
+        // while(sp<=pcount && sn<=ncount)
+        // {
+        //     if(flag==true){
+        //         arr[i]=a[i];
+        //         sn++;
+        //         i++;
+        //         flag=false;
+        //         // System.out.println(arr[i]);
+        //     }
+        //     else{
+        //         arr[i]=b[i];
+        //         sp++;
+        //         i++;
+        //         flag=true;
+        //         // System.out.println(arr[i]);
+        //     }
+        // }
+        // int i=0,sp=0,sn=0;
+        int res[] =new int[n];
+        // boolean flag = true;
+        // while(sp<=pcount&&sn<ncount){
+        //     if(flag==true){
+        //         res[i]=a[i];
+        //         sn++;
+        //         flag=false;
+        //     }
+        //     else{
+        //         res[i]=b[i];
+        //         sp++;
+        //         flag=true;
+        //     }
+
+        // }
+        // System.out.println("\n\n");
+        // System.out.println(i);
+        // System.out.println("\n\n");
+        // for(int j=0;j<n;j++){
+        //     System.out.println(arr[j]);
+        // }
 
         // while(sp<=pcount){
         //     arr[i]=b[sp];
@@ -62,17 +84,30 @@ public class assembleOrder {
         //     sn++;
         //     i++;
         // }
+        int i=0;
+        int k=0;
+        while(i!=pcount){
+            res[i]=b[k];
+            i+=2;
+            k++;
+        }
+        int j=1;
+        while(j!=ncount){
+            res[j]=a[k];
+            k++;
+            j+=2;
+        }
         System.out.println("\n\n");
         for(int j=0;j<n;j++){
-            System.out.println(arr[j]);
+            System.out.println(res[j]);
         }
-        // System.out.println("\n\n");
-        // for(int j=0;j<pcount;j++){
-        //     System.out.println(b[j]);
-        // }
-        // System.out.println("\n\n");
-        // for(int j=0;j<ncount;j++){
-        //     System.out.println(a[j]);
-        // }
+        System.out.println("\n\n");
+        for(int j=0;j<pcount;j++){
+            System.out.println(b[j]);
+        }
+        System.out.println("\n\n");
+        for(int j=0;j<ncount;j++){
+            System.out.println(a[j]);
+        }
     }
 }
